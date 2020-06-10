d051b49c943aa53b103841532632c5e01716c6f6
Maruf <maruf.csea1@gmail.com>
2020-06-10 18:30:50 -0400

    Partitioning updated

--- metis-5.1.0/libmetis/pmetis.c
+++ metis-5.1.0/libmetis/pmetis.c
@@ -161,7 +161,6 @@ idx_t MlevelRecursiveBisection(ctrl_t *ctrl, graph_t *graph, idx_t nparts,
   idx_t *label, *where;
   graph_t *lgraph, *rgraph;
   real_t wsum, *tpwgts2;
-printf("vertex found in the graph: %d\n", graph->nvtxs);
   if ((nvtxs = graph->nvtxs) == 0) {
     printf("\t***Cannot bisect a graph with 0 vertices!\n"
            "\t***You are trying to partition a graph into too many parts!\n");
